0aa2ee2842712a013a785055046e520b9c717f7d
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2025-03-18 15:03:17 -0500

    test CI/CD

---
+++ BlogSolution/Blog.Core/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Core.Entities;
+
+public abstract class BaseEntity<T>
+{
+    public T Id { get; protected set; }
+}
---
+++ BlogSolution/Blog.Core/Entities/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities;
+
+public class Blog : BaseEntity<long>
+{
+    public Blog()
+    {
+    }
+}
